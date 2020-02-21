ebb4b23c3e5e66d435219d4db285e29fb036126e
kellton <kellton@KELLHYDCPU151>
2020-02-21 17:22:37 +0530

    deploying Package for testing flexdeploy

---
+++ code/source/myNewFolder/schedular_jobs.java
@@ -0,0 +1,38 @@
+package myNewFolder;
+
+// -----( IS Java Code Template v1.2
+
+import com.wm.data.*;
+import com.wm.util.Values;
+import com.wm.app.b2b.server.Service;
+import com.wm.app.b2b.server.ServiceException;
+// --- <<IS-START-IMPORTS>> ---
+// --- <<IS-END-IMPORTS>> ---
+
+public final class schedular_jobs
+
+{
+	// ---( internal utility methods )---
+
+	final static schedular_jobs _instance = new schedular_jobs();
+
+	static schedular_jobs _newInstance() { return new schedular_jobs(); }
+
+	static schedular_jobs _cast(Object o) { return (schedular_jobs)o; }
+
+	// ---( server methods )---
+
+
+
+
+	public static final void test_cofig (IData pipeline)
+        throws ServiceException
+	{
+		// --- <<IS-START(test_cofig)>> ---
+		// @sigtype java 3.5
+		// --- <<IS-END>> ---
+
+                
+	}
+}
+
